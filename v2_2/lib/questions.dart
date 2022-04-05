0647b12059090ad8413d4908491b387d842c495d
Jonathan Dyason <jonathandyason@hotmail.com>
2022-04-05 12:24:44 +0200

    adding target blocks dynamically (start)

---
+++ v2_2/lib/questions.dart
@@ -0,0 +1,5 @@
+class Question1 {
+  String name = "CAT";
+  int letterNumber = 3;
+  List<String> answerBlocks = ["C", "A", "T"];
+}
--- v2_2/lib/target_blocks.dart
+++ v2_2/lib/target_blocks.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'LetterBlocks/z_block.dart';
+import 'questions.dart';
 
 class TargetBlocks extends StatefulWidget {
   @override
@@ -9,107 +9,44 @@ class TargetBlocks extends StatefulWidget {
 class _TargetBlocksState extends State<TargetBlocks> {
   Color backgroundColor = Color(0xff111111);
 
-  String? letter;
+  int letterBlockNumber = Question1().letterNumber;
 
-  //List<Widget?> letterBlocks = <Widget>[];
-  var letterBlocks = List<Widget?>.filled(3, null);
-  // var arr = new <Widget?>[](5);
-
-  Widget? letterBlock1;
-  Widget? letterBlock2;
-  Widget? letterBlock3;
+  var letterBlocks = List<Widget?>.filled(Question1().letterNumber, null);
 
   @override
   Widget build(BuildContext context) {
     return Row(
       children: [
-        DragTarget<Widget>(
-          builder: (
-            BuildContext context,
-            List<dynamic> accepted,
-            List<dynamic> rejected,
-          ) {
-            return Container(
-              margin: EdgeInsets.all(10),
-              height: 60.0,
-              width: 60.0,
-              decoration: BoxDecoration(
-                color: Colors.black,
-                borderRadius: BorderRadius.all(
-                  Radius.circular(10),
+        for (var i in letterBlocks)
+          DragTarget<Widget>(
+            builder: (
+              BuildContext context,
+              List<dynamic> accepted,
+              List<dynamic> rejected,
+            ) {
+              return Container(
+                margin: EdgeInsets.all(10),
+                height: 60.0,
+                width: 60.0,
+                decoration: BoxDecoration(
+                  color: Colors.black,
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(10),
+                  ),
+                  border: Border.all(
+                    color: Colors.white,
+                    width: 2.0,
+                  ),
                 ),
-                border: Border.all(
-                  color: Colors.white,
-                  width: 2.0,
-                ),
-              ),
-              child: letterBlocks[0],
-            );
-          },
-          onAccept: (data) {
-            setState(() {
-              letterBlocks[0] = data;
-            });
-          },
-        ),
-//         DragTarget<Widget>(
-//           builder: (
-//             BuildContext context,
-//             List<dynamic> accepted,
-//             List<dynamic> rejected,
-//           ) {
-//             return Container(
-//               margin: EdgeInsets.all(10),
-//               height: 60.0,
-//               width: 60.0,
-//               decoration: BoxDecoration(
-//                 color: Colors.black,
-//                 borderRadius: BorderRadius.all(
-//                   Radius.circular(10),
-//                 ),
-//                 border: Border.all(
-//                   color: Colors.white,
-//                   width: 2.0,
-//                 ),
-//               ),
-//               //      child: letterBlocks[1],
-//             );
-//           },
-//           onAccept: (data) {
-//             setState(() {
-//               //    letterBlocks[1] = data;
-//             });
-//           },
-//         ),
-//         DragTarget<Widget>(
-//           builder: (
-//             BuildContext context,
-//             List<dynamic> accepted,
-//             List<dynamic> rejected,
-//           ) {
-//             return Container(
-//               margin: EdgeInsets.all(10),
-//               height: 60.0,
-//               width: 60.0,
-//               decoration: BoxDecoration(
-//                 color: Colors.black,
-//                 borderRadius: BorderRadius.all(
-//                   Radius.circular(10),
-//                 ),
-//                 border: Border.all(
-//                   color: Colors.white,
-//                   width: 2.0,
-//                 ),
-//               ),
-//               //  child: letterBlocks[2],
-//             );
-//           },
-//           onAccept: (data) {
-//             setState(() {
-// //              letterBlocks[2] = data;
-//             });
-//           },
-//        ),
+                child: letterBlocks[0],
+              );
+            },
+            onAccept: (data) {
+              setState(() {
+                letterBlocks[0] = data;
+              });
+            },
+          ),
       ],
     );
   }
